3390ba943e1eda4313bb7e79e93f4dde24652900
Sankirna <Sankirnarana@gmail.com>
2019-10-10 15:07:07 -0400

    test

--- ClientApps/Admin/src/app/app.component.ts
+++ ClientApps/Admin/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from "@angular/forms";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Admin';
+   title = 'materialApp';
+   myControl = new FormControl();
+   states;
+   constructor(){
+      this.loadStates();
+   }
+   //build list of states as map of key-value pairs
+   loadStates() {
+      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+         Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+         Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+         Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+         North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+         South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+         Wisconsin, Wyoming';
+      this.states =  allStates.split(/, +/g).map( function (state) {
+         return {
+            value: state.toUpperCase(),
+            display: state
+         };
+      });
+   }
 }
--- ClientApps/Admin/src/app/app.module.ts
+++ ClientApps/Admin/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
