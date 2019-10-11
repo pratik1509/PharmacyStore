dc2706e3d8bc45981fda2577484b8e3a3511efa0
sankirna <sankirnarana@gmail.com>
2019-10-11 12:31:17 +0530

    angler materail layout set

--- ClientApps/Admin/src/app/app.component.ts
+++ ClientApps/Admin/src/app/app.component.ts
@@ -1,31 +1,16 @@
-import { Component } from '@angular/core';
-import { FormControl } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
-   selector: 'app-root',
-   templateUrl: './app.component.html',
-   styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-   title = 'materialApp';
-   myControl = new FormControl();
-   states;
-   constructor(){
-      this.loadStates();
-   }
-   //build list of states as map of key-value pairs
-   loadStates() {
-      var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-         Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-         Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-         Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-         North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-         South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-         Wisconsin, Wyoming';
-      this.states =  allStates.split(/, +/g).map( function (state) {
-         return {
-            value: state.toUpperCase(),
-            display: state
-         };
-      });
-   }
+export class AppComponent implements OnInit {
+    constructor(private translate: TranslateService) {
+        translate.setDefaultLang('en');
+    }
+
+    ngOnInit() {
+    }
 }
