9c7ab9fd63bc1397a74a5b28a6b4e768a77a8c84
sankirna <sankirnarana@gmail.com>
2019-10-17 19:07:25 +0530

    test

--- ClientApps/Admin/src/app/layout/doctor/list/list.component.ts
+++ ClientApps/Admin/src/app/layout/doctor/list/list.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, TemplateRef, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { PageEvent } from '@angular/material';
 import { DataAccessService } from '../../../services/data-access.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  displayedColumns: string[] = ['doctorName', 'address','actions'];
+  displayedColumns: string[] = ['doctorName', 'address', 'actions'];
   public dataSource = new MatTableDataSource();
 
   public paging: any = {};
 
-  constructor(public dataAccess: DataAccessService) {}
+  constructor(public dataAccess: DataAccessService, private dialog: MatDialog) {}
   ngOnInit() {
     this.getAllDoctors();
   }
@@ -29,6 +31,19 @@ export class ListComponent implements OnInit {
     this.paging.page = event.pageIndex + 1;
     this.getAllDoctors();
     return event;
+  }
+
+  deleteDialog(templateRef: TemplateRef<any>, doctorId: string): void {
+    const dialogRef = this.dialog.open(templateRef);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.dataAccess.post('Doctor/Delete?id=' + doctorId, {}).subscribe((data: any) => {
+          this.paging.page = 0;
+          this.getAllDoctors();
+        });
+      }
+    });
   }
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
--- ClientApps/Admin/src/app/layout/doctor/update/update.component.ts
+++ ClientApps/Admin/src/app/layout/doctor/update/update.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router,ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { DataAccessService } from '../../../services/data-access.service';
 
 @Component({
@@ -13,14 +13,19 @@ export class UpdateComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, public dataAccess: DataAccessService) {}
 
   ngOnInit() {
-    let doctorId = this.route.snapshot.params.id;
+    const doctorId = this.route.snapshot.params.id;
+    this.doctorForm = new FormGroup({
+      doctorId: new FormControl('', [Validators.required]),
+      doctorName: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required])
+    });
     this.dataAccess.get('Doctor/get?id=' + doctorId).subscribe((data: any) => {
-      this.doctorForm = new FormGroup({
-        doctorId: new FormControl(doctorId, [Validators.required]),
-        doctorName: new FormControl(data.data.doctorName, [Validators.required]),
-        address: new FormControl(data.data.address, [Validators.required])
+      this.doctorForm.patchValue({
+        doctorId: doctorId,
+        doctorName: data.data.doctorName,
+        address: data.data.address
       });
-    }); 
+    });
   }
 
   public hasError = (controlName: string, errorName: string) => {
@@ -31,8 +36,12 @@ export class UpdateComponent implements OnInit {
     this.router.navigate(['/doctor/list']);
   }
 
-  public update = (doctorFormValue) => {
-    let doctorUpdatePayload = { doctorId: doctorFormValue.doctorId, doctorName: doctorFormValue.doctorName, address: doctorFormValue.address};
+  public update = doctorFormValue => {
+    const doctorUpdatePayload = {
+      doctorId: doctorFormValue.doctorId,
+      doctorName: doctorFormValue.doctorName,
+      address: doctorFormValue.address
+    };
     if (this.doctorForm.valid) {
       this.dataAccess.post('Doctor/Update', doctorUpdatePayload).subscribe((data: any) => {
         this.router.navigate(['/doctor/list']);
